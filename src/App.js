abd0d7e4024111c433f9d4c92d3c30ffba4e8b22
LogiCule <rahuldubey7398@gmail.com>
2023-05-24 11:07:16 +0530

    base layout readdy

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import { Box } from "@mui/material";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import ExerciseDetail from "./components/ExerciseDetail";
+import Footer from "./components/Footer";
 
 export const App = () => {
-  return <div>App</div>;
+  return (
+    <Box width="400px" m="auto" sx={{ width: { xl: "1488px" } }}>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/exercise/:id" element={<ExerciseDetail />} />
+      </Routes>
+      <Footer />
+    </Box>
+  );
 };
 
 export default App;
---
+++ src/components/Navbar/index.js
@@ -0,0 +1,23 @@
+import { Stack } from "@mui/material";
+import React from "react";
+import { Link } from "react-router-dom";
+import Logo from "../../assets/images/Logo.png";
+const Navbar = () => {
+  return (
+    <Stack>
+      <Link to="/">
+        <img
+          src={Logo}
+          alt="logo"
+          style={{ width: "48px", height: "48px", margin: "0 20px" }}
+        />
+      </Link>
+      <Stack>
+        <Link to="/">Home</Link>
+        <Link to="/">Home</Link>
+      </Stack>
+    </Stack>
+  );
+};
+
+export default Navbar;
