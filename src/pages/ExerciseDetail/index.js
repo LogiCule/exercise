767e2bd03e3cbc217b3f9b7d8a64c6fdc205602b
LogiCule <rahuldubey7398@gmail.com>
2023-07-09 16:41:44 +0530

    exercise detail added

--- src/pages/ExerciseDetail/index.js
+++ src/pages/ExerciseDetail/index.js
@@ -1,7 +1,38 @@
-import React from "react";
+import { Box } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import Detail from "../../components/Detail";
+import ExerciseVideos from "../../components/ExerciseVideos/index.js";
+import SimilarExercises from "../../components/SimilarExercises";
+import { useParams } from "react-router-dom";
+import { exerciseOptions, fetchData } from "../../utils/fetchData";
 
 const ExerciseDetail = () => {
-  return <div>ExerciseDetail</div>;
+  const [exerciseDetail, setExerciseDetail] = useState({});
+  const { id } = useParams();
+  const fetchExerciseData = async () => {
+    const exerciseURL = "https://exercisedb.p.rapidapi.com";
+    const youtubeSearchURL =
+      "https://youtube-search-and-download.p.rapidapi.com";
+
+    const exerciseDetailData = await fetchData(
+      `${exerciseURL}/exercises/exercise/${id}`,
+      exerciseOptions
+    );
+    setExerciseDetail(exerciseDetailData);
+  };
+
+  useEffect(() => {
+    fetchExerciseData();
+  }, [id]);
+  console.log({ exerciseDetail });
+
+  return (
+    <Box>
+      <Detail exerciseDetail={exerciseDetail} />
+      <ExerciseVideos />
+      <SimilarExercises />
+    </Box>
+  );
 };
 
 export default ExerciseDetail;
